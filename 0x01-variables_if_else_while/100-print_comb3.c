41a0f9ab0cfa1d2e22c3907b800ad1167287fe1f
Emilvere <emiliaritachika@gmail.com>
2023-04-24 10:48:26 -0700

    comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		while (j <= '9')
 		{
-			if (!(i > j) || i == j)
+			if (i < j)
 			{
 				putchar(i);
 				putchar(j);
-				if (i == '8' && j == '9')
+				if (!(i == '8' && j == '9'))
 				{
 					putchar('\n');
 				}
@@ -31,7 +31,7 @@ int main(void)
 			j++;
 		}
 		j = '0';
-		i++; 
+		i++;
 	}
 	return (0);
 }
