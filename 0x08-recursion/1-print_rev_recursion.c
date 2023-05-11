f8b5514000f22dda777f5027c0018597a64d46bf
Emilvere <emiliaritachika@gmail.com>
2023-05-11 06:28:16 -0700

    print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,15 +3,12 @@
 /**
  * _print_rev_recursion -  prints a string in reverse
  * @s: string to be printed
- *
- * Return: nothing on success
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s);
-		++s;
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
